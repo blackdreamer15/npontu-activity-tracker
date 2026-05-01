67954ae08ec45e2352f6200923a09b4461fa8f62
Jessy <bakijessy@gmail.com>
2026-05-01 10:17:25 +0000

    feat: add routes for activity management & updates

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use Laravel\Fortify\Features;
+use App\Http\Controllers\ActivityController;
+use App\Http\Controllers\ActivityUpdateController;
 
 Route::inertia('/', 'welcome', [
     'canRegister' => Features::enabled(Features::registration()),
@@ -9,6 +11,17 @@ Route::inertia('/', 'welcome', [
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::inertia('dashboard', 'dashboard')->name('dashboard');
+    
+    Route::get('activities', [ActivityController::class, 'index']);
+    Route::post('activities', [ActivityController::class, 'store']);
+    Route::get('activities/{activity}', [ActivityController::class, 'show']);
+    Route::put('activities/{activity}', [ActivityController::class, 'update']);
+    Route::delete('activities/{activity}', [ActivityController::class, 'destroy']);
+
+    Route::get('activity-updates', [ActivityUpdateController::class, 'index']);
+    Route::post('activity-updates', [ActivityUpdateController::class, 'store']);
+
+    Route::get('reports', [ActivityController::class, 'report'])->name('activities.report');
 });
 
 require __DIR__.'/settings.php';
