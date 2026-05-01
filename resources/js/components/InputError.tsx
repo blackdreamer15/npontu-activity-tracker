cbf3e13ee12f798e712522ef24dbd486dccadfa9
Jessy <bakijessy@gmail.com>
2026-05-01 11:04:14 +0000

    feat: add InputError component

---
+++ resources/js/components/InputError.tsx
@@ -0,0 +1,17 @@
+import { ComponentPropsWithoutRef } from 'react';
+import { cn } from '@/lib/utils';
+
+export default function InputError({
+    message,
+    className = '',
+}: ComponentPropsWithoutRef<'span'> & {
+    message?: string;
+}) {
+    if (!message) return null;
+
+    return (
+        <span className={cn('text-sm font-medium text-destructive', className)}>
+            {message}
+        </span>
+    );
+}
