b4844943c8eb108390eae499810de76879787205
Jessy <bakijessy@gmail.com>
2026-05-01 10:36:47 +0000

    feat: add 'Activities' link to main nav in sidebar

--- resources/js/components/app-sidebar.tsx
+++ resources/js/components/app-sidebar.tsx
@@ -22,6 +22,11 @@ const mainNavItems: NavItem[] = [
         href: dashboard(),
         icon: LayoutGrid,
     },
+    {
+        title: 'Activities',
+        href: '/activities',
+        icon: FolderGit2,
+    },
 ];
 
 const footerNavItems: NavItem[] = [
